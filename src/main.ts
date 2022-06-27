6ce5b1858a8b440f766b86513c63ed9e2626ba0a
itoshusu <sopfantasy@yahoo.co.jp>
2022-06-27 20:18:28 +0900

    add: vue-gtagによるGA4導入

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,14 @@ import App from './App.vue'
 import router from './router'
 import './index.css'
 import { createPinia } from 'pinia'
+import VueGtag from "vue-gtag";
 
-createApp(App).use(router).use(createPinia()).mount('#app')
+createApp(App)
+  .use(router)
+  .use(VueGtag, {
+    config: { 
+      id: "G-CYS7Z53H7N",
+    },
+  }, router)
+  .use(createPinia())
+  .mount('#app')
