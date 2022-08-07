8fcac325f74f6ec74e622b2f843d874759aa8172
itoshusu <sopfantasy@yahoo.co.jp>
2022-08-07 21:47:52 +0900

    add: 動的ogp対応

--- src/components/button/TweetButton.vue
+++ src/components/button/TweetButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <a :href="`https://twitter.com/intent/tweet?&text=${store.tweetPreMessage}ゴリラが集めたバナナの本数は${store.score}本でした！%0Ahttps://gorilla-collecting-banana.netlify.app/%0A&hashtags=ゴリラのバナナ集め`" class="twitter-btn  text-xl" target="_blank" @click="pi">ツイートする</a>
+  <a :href="`https://twitter.com/intent/tweet?&text=${store.tweetPreMessage}ゴリラが集めたバナナの本数は${store.score}本でした！%0A&hashtags=ゴリラのバナナ集め%0Ahttps://gorilla-collecting-banana.netlify.app/?ogp=${store.ogpType}`" class="twitter-btn  text-xl" target="_blank" @click="pi">ツイートする</a>
 </template>
 
 <script setup lang="ts">
--- src/store/stageStore.ts
+++ src/store/stageStore.ts
@@ -7,6 +7,7 @@ interface StageState {
   tweetPreMessage: string
   collectionName: string
   gameMode: string
+  ogpType: string
 }
 
 export const useStageStore = defineStore('app', {
@@ -17,6 +18,7 @@ export const useStageStore = defineStore('app', {
     tweetPreMessage: '',
     collectionName: '',
     gameMode: '',
+    ogpType: ''
   }),
   actions: {
     setScore(score: number): void {
@@ -30,16 +32,19 @@ export const useStageStore = defineStore('app', {
       this.tweetPreMessage = 'ヘビと戯れながら'
       this.collectionName = 'normalRanking'
       this.gameMode = 'ノーマルモード'
+      this.ogpType = '1'
     },
     hardMode(): void {
       this.tweetPreMessage = '死神に追いかけられながら'
       this.collectionName = 'hardRanking'
       this.gameMode = 'ハードモード'
+      this.ogpType = '2'
     },
     extraMode(): void {
       this.tweetPreMessage = 'ドラゴンに攻撃されながら'
       this.collectionName = 'extraRanking'
       this.gameMode = 'エクストラモード'
+      this.ogpType = '3'
     }
 
   }
